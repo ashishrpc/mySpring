fc536d7bb17409e60d5baa2ff4f8f1bc2af22270
ashish.prasad <ashish.prasad@KMG-016.kmg.com>
2017-04-19 14:17:33 +0530

    mvc test

--- src/com/controller/HelloController.java
+++ src/com/controller/HelloController.java
@@ -6,17 +6,20 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
-
-public class HelloController implements Controller { 
-
+//import org.springframework.web.servlet.mvc.Controller;
+@Controller
+public class HelloController  { 
+	@RequestMapping(value="/index", method = RequestMethod.GET)
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         System.out.println("Returning hello view");
 
-        return new ModelAndView("hello.jsp");
+        return new ModelAndView("hello");
     }
 
 }
